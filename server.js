e14492fb9bddcc096489531445cfd90d76a3cff6
Diana Lescure <yo@dianitica.com>
2015-12-21 20:48:19 -0600

    Fixing hosts yet again... :rage4:

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Create the virtual hosts
 var homeHost = createVirtualHost("sjo.space", "homeland");
-var understandHost = createVirtualHost("sjo.space", "understand-javascript");
+var understandHost = createVirtualHost("understand.sjo.space", "understand-javascript");
 
 //Use the virtual hosts
 app.use(homeHost);
